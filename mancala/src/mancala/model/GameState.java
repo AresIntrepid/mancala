c6480a215d2235ebd694d3ee92d315d29cd6cfe4
Alan <longd@Alans-MacBook-Pro.local>
2025-11-22 21:25:14 -0800

    Implement mancala model

--- mancala/src/mancala/model/GameState.java
+++ mancala/src/mancala/model/GameState.java
@@ -4,6 +4,24 @@
 package mancala.model;
 
 public class GameState {
-    
-}
+  public int[] board;
+  public int currentTurn;
+  public int currentPlayer;
 
+  public GameState(int[] board, int currentTurn, int currentPlayer) {
+    this.board = board;
+    this.currentTurn = currentTurn;
+  }
+
+  public int getCurrentTurn() {
+    return currentTurn;
+  }
+
+  public int[] getBoard() {
+    return board;
+  }
+
+  public int getCurrentPlayer() {
+    return currentPlayer;
+  }
+}
