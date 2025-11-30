dd9d65c8580ceec18b6a30e80ba0bb6d58f2b014
Alan <longd@Alans-MacBook-Pro.local>
2025-11-29 21:58:09 -0800

    Implemented mancala pits rendering

--- mancala/src/mancala/view/BoardPanel.java
+++ mancala/src/mancala/view/BoardPanel.java
@@ -2,18 +2,22 @@
 // Agree on hit-testing approach and which geometry/metrics come from the style.
 package mancala.view;
 
-import javax.swing.*;
 import java.awt.*;
+import javax.swing.*;
 import mancala.style.BoardStyle;
 
 public class BoardPanel extends JPanel {
     private BoardStyle style;
-    
+    private int pitsPerSide;
+    private Pit[][] pits;
+
     public BoardPanel() {
+        pitsPerSide = 6;
+        initializePits();
         setPreferredSize(new Dimension(800, 400));
         setBackground(Color.LIGHT_GRAY);
     }
-    
+
     public void setStyle(BoardStyle style) {
         this.style = style;
         if (style != null) {
@@ -21,11 +25,11 @@ public class BoardPanel extends JPanel {
         }
         repaint();
     }
-    
+
     public BoardStyle getStyle() {
         return style;
     }
-    
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -47,14 +51,34 @@ public class BoardPanel extends JPanel {
         g2d.setColor(getForeground());
         g2d.setStroke(new BasicStroke(2));
         g2d.drawRoundRect(rectX, rectY, rectWidth, rectHeight, 20, 20);
-        
-        // Draw placeholder text
-        g2d.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 24));
-        FontMetrics fm = g2d.getFontMetrics();
-        String text = "Board Area";
-        int textX = (width - fm.stringWidth(text)) / 2;
-        int textY = height / 2 + fm.getAscent() / 2;
-        g2d.setColor(Color.WHITE);
-        g2d.drawString(text, textX, textY);
+
+        int padding = width / 64;
+        int pitSpacing = width / 64;
+        int xStart = rectX + padding;
+        int yStart = rectY + padding;
+        int pitWidth = (rectWidth - padding * 2 - pitSpacing * (pitsPerSide - 1)) / pitsPerSide;
+        int pitHeight = (rectHeight - padding * 2 - pitSpacing) / 2;
+        int pitSize = Math.min(pitWidth, pitHeight);
+
+
+        for (int i = 0; i < 2; ++i) {
+            for (int j = 0; j < pitsPerSide; ++j) {
+                int x = xStart + j * (pitWidth + pitSpacing);
+                int y = yStart + i * (pitHeight + pitSpacing);
+
+                pits[i][j].draw(g2d, x, y, pitSize, style.getBackgroundColor());
+            }   
+        }
+
+    }
+
+    private void initializePits() {
+        pits = new Pit[2][pitsPerSide];
+
+        for (int i = 0; i < 2; ++i) {
+            for (int j = 0; j < pitsPerSide; ++j) {
+                pits[i][j] = new Pit(4);
+            }
+        }
     }
 }
